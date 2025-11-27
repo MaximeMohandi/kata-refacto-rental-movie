24023cb5a9e82c7f6c9d7b1f6d43bf081fe9260f
Maxime Mohandi <mohandi.maxime@gmail.com>
2025-11-27 12:22:51 +0100

    refactor: 💡 extract metdho determine rental price

--- csharp/MovieRental/Customer.cs
+++ csharp/MovieRental/Customer.cs
@@ -26,37 +26,20 @@ namespace MovieRental
             int frequentRenterPoints = 0;
             var statement = new StringBuilder("Rental Record for " + _name + "\n");
 
-            foreach (Rental each in _rentals)
+            foreach (Rental rental in _rentals)
             {
-                double thisAmount = 0;
+                double rentalPrice = 0;
 
                 //determine amounts for each line
-                switch (each.getMovie().getPriceCode())
-                {
-                    case Movie.REGULAR:
-                        thisAmount += 2;
-                        if (each.getDaysRented() > 2)
-                            thisAmount += (each.getDaysRented() - 2) * 1.5;
-                        break;
-                    case Movie.NEW_RELEASE:
-                        thisAmount += each.getDaysRented() * 3;
-                        break;
-                    case Movie.CHILDRENS:
-                        thisAmount += 1.5;
-                        if (each.getDaysRented() > 3)
-                            thisAmount += (each.getDaysRented() - 3) * 1.5;
-                        break;
-                }
+                rentalPrice = DetermineRentalPrice(rental, rentalPrice);
 
-                // add frequent renter points
-                frequentRenterPoints++;
+
                 // add bonus for a two day new release rental
-                if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                    frequentRenterPoints++;
+                frequentRenterPoints += ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) ? 2 : 1;
 
                 // show figures for this rental
-                statement.Append("\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n");
-                totalAmount += thisAmount;
+                statement.Append("\t" + rental.getMovie().getTitle() + "\t" + rentalPrice + "\n");
+                totalAmount += rentalPrice;
             }
 
             // add footer lines
@@ -64,6 +47,28 @@ namespace MovieRental
             statement.Append("You earned " + frequentRenterPoints + " frequent renter points");
 
             return statement.ToString();
+        }
+
+        private static double DetermineRentalPrice(Rental each, double thisAmount)
+        {
+            switch (each.getMovie().getPriceCode())
+            {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+
+            return thisAmount;
         }
     }
 
